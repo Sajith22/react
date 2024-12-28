ef040c5e4bf8b27dc914171d3b277abb21a07e9b
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2024-12-29 02:14:19 +0530

    Video 04 is Done!

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import Employee from './components/Employee';
 
 
 function App() {
-  console.log('we are about ot list the emmployees');
   const showEmployee=true;
   return (
     <div className="App">
       {showEmployee ?
         <>
-        <Employee/>
-        <Employee/>
-        <Employee/>
-        <Employee/>
-        <Employee/>
+        <Employee name="Caleb" role="Intern"/>
+        <Employee name="Abby"/>
+        <Employee name="John"/>
         </>
       : 
       <p>You can't see the employee</p>
